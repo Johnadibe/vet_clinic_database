418826bfcbcb954af37ecc1082c81761a6623621
John Adibe <johnadibe123@gmail.com>
2023-03-31 11:45:10 -0800

    Update ending date_of_birth on Line 5 in queries.sql file as requested by the code reviewer

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 
 -- PROJECT 1
 SELECT * FROM animals WHERE name ILIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN DATE '2016-01-01' AND '2019-01-01';
+SELECT name FROM animals WHERE date_of_birth BETWEEN DATE '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.50;
