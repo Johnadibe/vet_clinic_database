60d2cbb8a2791dadc46cd7ab19a2f5e71bb923cc
John Adibe <johnadibe123@gmail.com>
2023-04-01 10:14:37 -0800

    Query multiple tables

--- schema.sql
+++ schema.sql
@@ -12,3 +12,23 @@ CREATE TABLE animals (
 
 -- PROJECT 2
 ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
+-- PROJECT 3
+CREATE TABLE owners (
+ id BIGSERIAL PRIMARY KEY,
+ full_name VARCHAR(255),
+ age INTEGER
+);
+
+CREATE TABLE species (
+ id BIGSERIAL PRIMARY KEY,
+ name VARCHAR(255)
+);
+
+ALTER TABLE animals DROP COLUMN id;
+ALTER TABLE animals ADD COLUMN id BIGSERIAL PRIMARY KEY; 
+ALTER TABLE animals DROP COLUMN species; 
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id); 
+ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(id);
+
+-- PROJECT 4
